e63d1c7b0a6a1ff81bc274c09ab76be30ec894d6
Arceus-sj <71997730+Arceus-sj@users.noreply.github.com>
2020-11-13 19:31:48 +0530

    (if - else) condition Implementation

---
+++ Python/Program7.py
@@ -0,0 +1,19 @@
+# Example1:
+# if-else condition
+age = int(input('Enter your age: '))
+if age > 18:
+    print('you can vote!')
+else:
+    print("you can't vote!")
+# --------------------------------
+
+# Example2:
+# if-else-if condition:
+
+temp = float(input('Temprature: '))
+if temp > 30:
+    print('Temprature is Hot! ')
+elif temp >= 15:
+    print('Temprature is Normal! ')
+else:
+    print('Temprature is cold! ')
