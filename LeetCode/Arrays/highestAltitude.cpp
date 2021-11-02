2b2f588652b1aa6f9cdd7fcfc6b545d2e283b74a
Shantanu Jana <71997730+Arceus-sj@users.noreply.github.com>
2021-11-02 12:04:14 +0530

    1732 | Find the Highest Altitude

--- LeetCode/Arrays/highestAltitude.cpp
+++ LeetCode/Arrays/highestAltitude.cpp
@@ -4,27 +4,35 @@
 using namespace std;
 
 int largestAltitude(vector<int>& gain) {
-    int n = gain.size();
-    int maxAlt = 0, altitude = 0;
-    for(int i = 0; i < n; i++) {
-        altitude = altitude + gain[i];
-        if(maxAlt < altitude)
-            maxAlt = altitude;
-    }
-    
-    return maxAlt;
+	int n = gain.size();
+	vector<int> trip(n+1);
+	 
+	int altitude = 0;
+	for(int i = 1; i < n+1; i++) {
+		trip[i] = trip[i-1] + gain[i-1];
+	}
+	
+	for(int i = 0; i < n+1; i++) {
+		if(altitude < trip[i])
+			altitude = trip[i];
+	}
+	return altitude;
 }
 
 int main() {
-    int n;
-    cin >> n;
-    vector<int> gain;
-    int a;
-    for(int i = 0; i < n; i++) {
-        cin >> a;
-        gain.push_back(a);
-    }
-    
-    cout << largestAltitude(gain) << "\n";
-    return 0;
+	int n;
+	cin >> n;
+	
+	vector<int> gain;
+	int val;
+	for(int i = 0; i<n; i++) {
+		cin >> val;
+		gain.push_back(val);
+	}
+	
+	int ans = largestAltitude(gain);
+	
+	cout << ans << "\n"; 
+	
+	return 0;
 }
