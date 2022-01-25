6546b7d3a0ef9b6276d04e6e52de94e47b53dfbb
Shantanu Jana <71997730+Arceus-sj@users.noreply.github.com>
2022-01-25 22:23:02 +0530

    1295 | Find Numbers with Even Number of Digits

--- LeetCode/Arrays/findEvenDigit.cpp
+++ LeetCode/Arrays/findEvenDigit.cpp
@@ -1,33 +1,68 @@
 #include<iostream>
 #include<vector>
+#include<cstring>
+#include<sstream>
+#include<typeinfo>
 #include<cmath>
 
 using namespace std;
 
-int findNumber(vector<int>& nums) {
-    int n = nums.size();
-    int countEven = 0;
-    for(int i = 0; i < n; i++) {
-        int num = floor(log10(nums[i]) + 1);//count total digits
-        if(num % 2 == 0)
-            countEven++;
-        else 
-            continue;
-    }
-    
-    return countEven;
+/*
+int findNumbers(vector<int>& nums) {
+	int size = nums.size(); 
+	
+	int countEvenDigits = 0;
+	
+	for(int i = 0; i < size; i++) {
+		
+		stringstream ss;
+		
+		ss << nums[i];
+		
+		string number;
+		
+		ss >> number;
+		
+		//cout << "Digit " << number << " | size " << number.length() << "\n"; 
+		
+		if(number.length() % 2 == 0)
+			countEvenDigits++;
+		
+	}
+	
+	return countEvenDigits;
+	
+}
+*/
+
+//optimized-method
+int findNumbers(vector<int>& nums) {
+	int size = nums.size();
+	
+	int countEvenDigit = 0;
+	for(int i = 0; i < size; i++) {
+		
+		int num = floor(log10(nums[i]) + 1);
+		if(num % 2 == 0)
+			countEvenDigit++;
+	}
+	
+	return countEvenDigit;
+	
 }
 
 int main() {
-    int n;
-    cin >> n;
-    vector<int> nums;
-    int a;
-    for(int i = 0; i < n; i++) {
-        cin >> a;
-        nums.push_back(a);
-    }
-    
-    cout << findNumber(nums) << "\n";
-    return 0;
+	int n;
+	cin >> n;
+	
+	vector<int> nums;
+	int it;
+	for(int i = 0;i < n; i++) {
+		cin >> it;
+		nums.push_back(it);
+	}
+	
+	
+	cout << findNumbers(nums) << "\n";
+	return 0;
 }
